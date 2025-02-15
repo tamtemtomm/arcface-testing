ffff6a5d4a365fe150ef0b476999d8860a9a4246
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2025-02-15 10:37:52 +0700

    Add identity labeling

--- process_img.py
+++ process_img.py
@@ -1,9 +1,9 @@
 import json, torch
+from torch import nn
 import torchvision.transforms as T
 from utils.tools import *
 from mtcnn.tools import *
 from model import *
-from sklearn.metrics.pairwise import cosine_similarity
 from config import REFERENCE_POINT
 
 class Pipeline : 
@@ -11,11 +11,12 @@ class Pipeline :
         self.mtcnn = initialize_mtcnn(device=device)
         self.arcface = initialize_arcface(device=device)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.pairwise = nn.PairwiseDistance(p=2)
         
         with open(facebank_path, "r") as outfile: 
             self.facebank = json.load(outfile)
     
-    def process_face(self, img, box, point):
+    def recognize_face(self, img, box, point):
         cropped_img = mtcnn_crop(img, box)
         normalized_point = normalize_point(point, box)
         
@@ -23,17 +24,18 @@ class Pipeline :
         input_tensor = img2tensor(aligned_face)
         
         with torch.no_grad():
-            embeddings = self.arcface(input_tensor)
+            embedding = self.arcface(input_tensor)
         
-        return embeddings
+        name = self.estimate_face(embedding=embedding)
+        return name
 
-    def recognize_face(self, embedding):
+    def estimate_face(self, embedding):
         best_match = None
         best_score = -1
 
         for name, known_embedding in self.facebank.items():
             # Calculate cosine similarity
-            similarity = cosine_similarity(embedding.cpu().numpy(), np.array(known_embedding).reshape(1, -1))[0][0]
+            similarity = self.pairwise(embedding, torch.tensor(known_embedding))[0][0]
 
             if similarity > best_score:
                 best_score = similarity
@@ -55,29 +57,18 @@ class Pipeline :
             img = img2np(img)
             
         results = mtcnn_detect(img, self.mtcnn)
-        
+                        
         annotated_img = mtcnn_annotate(img, results['boxes'], results['points'])
+        
+        for box, point in zip(results['boxes'], results['points']):
+            
+            identity = self.recognize_face(img, box, point)
+            x1, y1, x2, y2 = map(int, box)
+            cv2.putText(annotated_img, f"{identity}", (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        
         annotated_img = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
         return annotated_img
-
-    # def process_img2(self, img) : 
-    #     results = mtcnn_detect(img, self.mtcnn)
-
-    #     if results['boxes'] is not None and results['points'] is not None:
-    #         annotated_img = img.copy()
-
-    #         for box, point in zip(results['boxes'], results['points']):
-    #             # Get embedding for detected face
-    #             embedding = self.process_face(img, box, point)
-    #             identity = self.recognize_face(embedding)
-
-    #             # Draw annotation
-    #             x1, y1, x2, y2 = map(int, box)
-    #             cv2.rectangle(annotated_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    #             cv2.putText(annotated_img, f"{identity}", (x1, y1 - 10),
-    #                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-
-    #         return annotated_img
     
     def process_camera(self):
         cap = cv2.VideoCapture(0)  # Open webcam (0 for default camera)
@@ -92,10 +83,12 @@ class Pipeline :
                 print("Error: Failed to capture frame.")
                 break
             
-            try : 
-                annotated_frame = self.process_img(frame)
-            except : 
-                annotated_frame = frame
+            annotated_frame = self.process_img(frame)
+            
+            # try : 
+            #     annotated_frame = self.process_img(frame)
+            # except : 
+            #     annotated_frame = frame
 
             cv2.imshow("Live Face Detection", annotated_frame)
 
